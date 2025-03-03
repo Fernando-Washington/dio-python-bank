72907528955ed81eb5eab7dc010a8fcdfb05478f
Fernando Washington <ironitabarbosa@gmail.com>
2025-03-03 19:38:20 -0400

    add resources

---
+++ resources.py
@@ -0,0 +1,14 @@
+def msg(text):
+    print("-" * 46)
+    print(text.center(46))
+    print("-" * 46)
+
+def menu():
+    msg('Bem Vindo!')
+    print('-----------------')
+    print('- 1 - Depósito  -')
+    print('- 2 - Saque     -')
+    print('- 3 - Extrato   -')
+    print('- 4 - Sair      -')
+    print('-----------------')
+
